611069734afcc9f1cb3a293165762146e9da0dc6
NS Park <55871121+ks1466@users.noreply.github.com>
2023-04-14 18:01:59 +0900

    플레이어
    x축 이동 , 스케일을 활용한 이미지 반전

--- ClimbCloud/Assets/Scripts/PlayerController.cs
+++ ClimbCloud/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rigid2D;
     float jumpForce = 680.0f;
+    float walkForce = 30.0f;
+    float maxWalkSpeed = 2.0f;
 
     void Start()
     {
@@ -18,5 +20,23 @@ public class PlayerController : MonoBehaviour
         {
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
+
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        float speedx = Mathf.Abs(this.rigid2D.velocity.x);
+
+        if( speedx < this.maxWalkSpeed )
+        {
+            this.rigid2D.AddForce(transform.right * key * this.walkForce);
+        }
+
+        if( key != 0 )
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+                
     }
+
 }
