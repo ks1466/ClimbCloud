89a3385b81538989f560f3cdaac3f5784cdd8cb3
NS Park <55871121+ks1466@users.noreply.github.com>
2023-04-19 12:43:47 +0900

    버그 수정 및 점프 애니메이션추가

--- ClimbCloud/Assets/Scripts/PlayerController.cs
+++ ClimbCloud/Assets/Scripts/PlayerController.cs
@@ -11,6 +11,9 @@ public class PlayerController : MonoBehaviour
     float walkForce = 30.0f;
     float maxWalkSpeed = 2.0f;
 
+    //스마트폰 조작
+    float threshold = 0.2f;
+
     void Start()
     {
         this.rigid2D = GetComponent<Rigidbody2D>();
@@ -19,8 +22,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        //스마트폰 조작
+        /*
+        if( Input.GetMouseButtonDown(0) && this.rigid2D.velocity.y == 0)
         {
+            this.rigid2D.AddForce(transform.up * this.jumpForce);
+        }
+        int key =0;
+        
+        if (Input.acceleration.x > threshold) key = 1;
+        if (Input.acceleration.x < -threshold) key = -1;
+        */
+        //스마트폰 조작 end
+
+        if (Input.GetKeyDown(KeyCode.Space) && this.rigid2D.velocity.y == 0)
+        {
+            this.animator.SetTrigger("JumpTrigger");
             this.rigid2D.AddForce(transform.up * this.jumpForce);
         }
 
@@ -40,11 +57,24 @@ public class PlayerController : MonoBehaviour
             transform.localScale = new Vector3(key, 1, 1);
         }
 
-        this.animator.speed = speedx / 2.0f;
+        //Walk , Jump 각각의 애니메이션 속도를 변경함
+        if(this.rigid2D.velocity.y == 0)
+        {
+            this.animator.speed = speedx / 2.0f;
+        }
+        else
+        {
+            this.animator.speed = 1.0f;
+        }
 
         if( this.animator.speed == 0 )
         {
+            //애니메이션 첫번째로 돌리고싶음
+        }
 
+        if( transform.position.y < -10 )
+        {
+            SceneManager.LoadScene("GameScene");
         }
 
     }
